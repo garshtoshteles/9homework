050750ae0daec7a5a5d887a5aaa9a1ab1c571f62
garshtoshteles <tj.yuxn.scott@gmail.com>
2020-08-21 19:09:55 -0400

    dropped necessary files from homework folder into working folder. first comm

---
+++ app.js
@@ -0,0 +1,123 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+console.log("Hi, welcome to Node Pizza");
+
+var questions = [
+  {
+    type: "confirm",
+    name: "toBeDelivered",
+    message: "Is this for delivery?",
+    default: false,
+  },
+  {
+    type: "input",
+    name: "phone",
+    message: "What's your phone number?",
+    validate: function (value) {
+      var pass = value.match(
+        /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
+      );
+      if (pass) {
+        return true;
+      }
+
+      return "Please enter a valid phone number";
+    },
+  },
+  {
+    type: "list",
+    name: "size",
+    message: "What size do you need?",
+    choices: ["Large", "Medium", "Small"],
+    filter: function (val) {
+      return val.toLowerCase();
+    },
+  },
+  {
+    type: "input",
+    name: "quantity",
+    message: "How many do you need?",
+    validate: function (value) {
+      var valid = !isNaN(parseFloat(value));
+      return valid || "Please enter a number";
+    },
+    filter: Number,
+  },
+  {
+    type: "expand",
+    name: "toppings",
+    message: "What about the toppings?",
+    choices: [
+      {
+        key: "p",
+        name: "Pepperoni and cheese",
+        value: "PepperoniCheese",
+      },
+      {
+        key: "a",
+        name: "All dressed",
+        value: "alldressed",
+      },
+      {
+        key: "w",
+        name: "Hawaiian",
+        value: "hawaiian",
+      },
+    ],
+  },
+  {
+    type: "rawlist",
+    name: "beverage",
+    message: "You also get a free 2L beverage",
+    choices: ["Pepsi", "7up", "Coke"],
+  },
+  {
+    type: "input",
+    name: "comments",
+    message: "Any comments on your purchase experience?",
+    default: "Nope, all good!",
+  },
+  {
+    type: "list",
+    name: "prize",
+    message: "For leaving a comment, you get a freebie",
+    choices: ["cake", "fries"],
+    when: function (answers) {
+      return answers.comments !== "Nope, all good!";
+    },
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {});
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
