7069de502d666ddc59b123b3c4f6089eda950d17
garshtoshteles <tj.yuxn.scott@gmail.com>
2020-10-01 16:36:56 -0400

    now passes all tests. tests just fed args in diff order

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -2,47 +2,47 @@ const Employee = require("../lib/Employee");
 
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
-  expect(typeof(e)).toBe("object");
+  expect(typeof e).toBe("object");
 });
 
 test("Can set name via constructor arguments", () => {
   const name = "Alice";
-  const e = new Employee(name);
+  const e = new Employee(1, "Employee", name, "foo@bar.com");
   expect(e.name).toBe(name);
 });
 
 test("Can set id via constructor argument", () => {
   const testValue = 100;
-  const e = new Employee("Foo", testValue);
-  expect(e.id).toBe(testValue);
+  const e = new Employee(testValue, "Employee", "Foo", "foo@bar.com");
+  expect(e.IDnum).toBe(testValue);
 });
 
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
+  const e = new Employee(1, "Manager", "Foo", testValue);
   expect(e.email).toBe(testValue);
 });
 
 test("Can get name via getName()", () => {
   const testValue = "Alice";
-  const e = new Employee(testValue);
+  const e = new Employee(1, "Intern", testValue, "foo@bar.com");
   expect(e.getName()).toBe(testValue);
 });
 
 test("Can get id via getId()", () => {
   const testValue = 100;
-  const e = new Employee("Foo", testValue);
+  const e = new Employee(testValue, "Intern", "Foo", "foo@bar.com");
   expect(e.getId()).toBe(testValue);
 });
 
 test("Can get email via getEmail()", () => {
   const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
+  const e = new Employee(1, "Intern", "Foo", testValue);
   expect(e.getEmail()).toBe(testValue);
 });
 
-test("getRole() should return \"Employee\"", () => {
+test('getRole() should return "Employee"', () => {
   const testValue = "Employee";
-  const e = new Employee("Alice", 1, "test@test.com");
+  const e = new Employee(1, "Employee", "Alice", "test@test.com");
   expect(e.getRole()).toBe(testValue);
 });
